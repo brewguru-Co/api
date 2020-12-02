1512d3de8cc156efcf7f886831fde070f10856dc
MincheolC <mccha0407@gmail.com>
2020-12-03 03:18:34 +0900

    fix: remove auto refreshing token and maxAge

--- middlewares/token.js
+++ middlewares/token.js
@@ -1,4 +1,4 @@
-const { decodeToken, generateToken } = require('../helpers/token');
+const { decodeToken } = require('../helpers/token');
 
 const tokenMiddleware = async (req, res, next) => {
   const token = req.cookies.access_token;
@@ -8,11 +8,6 @@ const tokenMiddleware = async (req, res, next) => {
   try {
     const decoded = await decodeToken(token);
 
-    if (Date.now() / 1000 - decoded.iat > 60 * 60 * 2) {
-      const { _id, id } = decoded;
-      const freshToken = await generateToken({ _id, id });
-      res.cookie('access_token', freshToken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
-    }
     req.user = {
       id: decoded.id,
     };
